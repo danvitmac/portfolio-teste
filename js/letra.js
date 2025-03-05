3bd3f3fb3a9137c45e30e8e67c0d73aae528535c
danvitmac <daniel.atdr@gmail.com>
2025-03-05 21:49:34 +0000

    change code in index

--- js/letra.js
+++ js/letra.js
@@ -1,6 +1,6 @@
 /* ----- TYPING EFFECT ----- */
 let typingEffect = new Typed(".typedText", {
-    strings: ["Daniel Viturino Machado","Programador","Desenvolvedor" ],
+    strings: ["Daniel Machado","Programador","Desenvolvedor" ],
     loop: true,
     typeSpeed: 100,
     backSpeed: 80,
